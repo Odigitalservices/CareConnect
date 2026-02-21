47075cb089eeea6fc5dc67a0a4fe3009797b5b94
walid <walid@WalidMacbookPro.local>
2026-02-21 16:09:21 +0000

    feat: add Angular professionals routing and nav link

--- web-admin/src/app/app.routes.ts
+++ web-admin/src/app/app.routes.ts
@@ -3,5 +3,12 @@ import { LoginComponent } from './features/auth/login.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  {
+    path: 'professionals',
+    loadComponent: () =>
+      import('./features/professionals/professionals-list.component').then(
+        (m) => m.ProfessionalsListComponent
+      ),
+  },
+  { path: '', redirectTo: 'professionals', pathMatch: 'full' },
 ];
